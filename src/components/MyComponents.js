fdbf0c995b3736d5dda333e4efa699150319bcc4
TuanCanhh <128canhnguyen9@gmail.com>
2025-01-17 23:15:40 +0700

    video19

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -7,16 +7,26 @@ class MyComponents extends React.Component{
 
     state = {
         name: ' canh',
-        address: 'hn',
+        address: 'hn ',
         age: 23
     };
+
+    handleClick(even){
+        console.log(" click me my button")
+        console.log("My name is", this.state.name )
+    }
+
+    handleOnMoverOver(even) {
+        console.log(even.pageX)
+    }
 
     // JSX
     render(){
         return (
             <div> 
                     My name is {this.state.name} and form  {this.state.address}
-
+                    <button onMouseOver = {this.handleOnMoverOver}>hover me</button>
+                    <button onClick = {this.handleClick}>click me</button>
             </div>
         );
     }
