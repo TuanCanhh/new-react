7709b70a324a342b222f1cff2fafc84b42c82382
TuanCanhh <128canhnguyen9@gmail.com>
2025-01-17 12:20:05 +0700

    video 16

---
+++ src/components/MyComponents.js
@@ -0,0 +1,19 @@
+// class components
+// fuctioon components
+
+import React from "react";
+
+class MyComponents extends React.Component{
+    // JSX
+    render(){
+        return (
+            <div>my first components
+                asasdasdasdasdasdasd{
+                        Math.random()
+                }
+            </div>
+        );
+    }
+}
+
+export default MyComponents;
